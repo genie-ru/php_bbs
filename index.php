feb76df1848b963911b6dc403580e10961133888
genie-ru <genie.zgmf987@gmail.com>
2024-03-13 17:54:05 +0900

    HTML雛型作成

--- index.php
+++ index.php
@@ -1,2 +1,36 @@
-<?php
-echo "Hello World";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>掲示板</title>
+</head>
+<body>
+    <header>
+        <h1 class="title">掲示板</h1>
+        <hr>
+    </header>
+
+    <div class="threadWrapper">
+        <div class="childWrapper">
+            <div class="threadTitle">
+                <span>スレッドタイトル</span>
+                <h1>掲示板を作ってみた</h1>
+            </div>
+            <section>
+                <article>
+                    <div class="wrapper">
+                        <div class="nameArea">
+                            <span>名前:</span>
+                            <p class="username">GenieCode</p>
+                            <time>:2024/3/14 17:51</time>
+                        </div>
+                        <p class="comment">手書きのコメントです。</p>
+                    </div>
+                </article>      
+            </section>
+        </div>
+    </div>
+</body>
+</html>
+
