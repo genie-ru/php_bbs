a2dbda60252c0c1210cbc3a6c43961319b411a33
genie-ru <genie.zgmf987@gmail.com>
2024-03-13 19:28:17 +0900

    DB 表示

--- index.php
+++ index.php
@@ -12,6 +12,15 @@ if(isset($_POST["submitButton"])) {
     $body = $_POST['body'];
     var_dump($body);
 }
+$comment_array = array();
+//コメントデータをテーブルから取得してくる
+$sql = "SELECT * FROM comment";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+
+$comment_array = $statement;
+
+var_dump($comment_array->fetchObject());
 ?>
 
 
