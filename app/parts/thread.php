1bcc1716acca3d89844d9552071b8fdedf5d1910
genie-ru <genie.zgmf987@gmail.com>
2024-03-17 19:38:45 +0900

    リファクタリング

--- app/parts/thread.php
+++ app/parts/thread.php
@@ -49,29 +49,8 @@ $comment_array = $statement;
                 <span>スレッドタイトル</span>
                 <h1>掲示板を作ってみた</h1>
             </div>
-            <section>
-                <?php foreach($comment_array as $comment) :?>
-                    <article>
-                        <div class="wrapper">
-                            <div class="nameArea">
-                                <span>名前:</span>
-                                <p class="username"><?php echo $comment["username"]; ?></p>
-                                <time>:<?php echo $comment["post_date"]; ?></time>
-                            </div>
-                            <p class="comment"><?php echo $comment["body"]; ?></p>
-                        </div>
-                    </article>   
-                <?php endforeach ?>    
-            </section>
-            <form class="formWrapper" method="POST">
-                <div>
-                    <input type="submit" value="書き込む" name="submitButton">
-                    <label>名前:</label>
-                    <input type="text" name="username">
-                </div>
-                <div>
-                    <textarea class="commentTextArea" name="body"></textarea>
-                </div>
-            </form>
+            <?php include("commentSection.php"); ?>
+            <?php include("commentForm.php"); ?>
         </div>
     </div>
+
